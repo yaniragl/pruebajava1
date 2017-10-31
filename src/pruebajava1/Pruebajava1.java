3145c88fb3d8b9b6042fbbb589b0a564426b842e
yaniragl <yanira_galeas@hotmail.com>
2017-10-30 21:12:14 -0300

    Esto crea un repositorio

---
+++ src/pruebajava1/Pruebajava1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+
+
+/**
+ * Yanira Galeas Rojas
+ * @author ceduc
+ * Fecha: 30 10 2017
+ * Programa:PruebaJava1
+ * Responsabilidad:Clase principal
+ *****************************************************/
+public class Pruebajava1 
+       
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
