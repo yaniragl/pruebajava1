6b4af8fdede641c00aa635ab0f91543d26527677
yaniragl <yanira_galeas@hotmail.com>
2017-10-30 21:51:33 -0300

    Implementar programa de edad y nombre

---
+++ src/ProgramaNomEdad.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ceduc
+ */
+import java.util.Scanner;
+public class ProgramaNomEdad 
+ 
+{
+   public static void main(String[] args)
+            
+   {
+      Scanner sc = new Scanner (System.in);
+               
+   System.out.println("por favor ingrese su nombre");
+   System.out.println("por favor ingrese su edad");
+       int Edad = 0;
+   System.out.println("En 20 años tendra"+(Edad+20));
+   
+     int edad = sc.nextInt();    
+     System.out.println("Bienvenido"(nombre");
+   }      
