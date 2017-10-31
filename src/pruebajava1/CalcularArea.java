877493482f4875b0264f86750803792d67c7e146
yaniragl <yanira_galeas@hotmail.com>
2017-10-30 21:43:05 -0300

    Calcular area del rectangulo

---
+++ src/pruebajava1/CalcularArea.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ceduc
+ */
+public class CalcularArea 
+{
+   int area;//se declara las variables
+   int base;
+   int altura;
+   
+   public static void main(String[] args)
+                 
+    
+   {    
+    
+ System.out.println("ingrese valor altura");
+ System.out.println("ingrese valor Base");
+  System.out.println("calcular=base*altura");
+  System.out.println("calcular area");
+  System.out.println("El area  del rectangulo es +area");
+  
+  String showImputDialog;
+  
+  String showInputDialog= JOptionPane.showInputDialog(null,"calcula el area=base*altura");
+        
+  }
+   
+       
+   }
+       
+   
+      
+  
+   
+
+        
+        
+        
+
+      
+            
+          
+        
+           
+
